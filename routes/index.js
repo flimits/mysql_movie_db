4483633ac607d1c9ddba0fc492fd2c285ab3c2dc
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-10-30 14:03:26 -0700

    correct typos

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,15 @@ const miniApp = require('express').Router();
 
 // Import our modular routers for /tips and /feedback
 const moviesRouter = require('./movies');
-const movie-reviewsRouter = require('./movie-reviews');
-const add-movieRouter = require('./add-movie');
+const movie_reviewsRouter = require('./movie-reviews');
+const add_movieRouter = require('./add-movie');
 const movieRouter = require('./movie');
 const reviewRouter = require('./review');
 
 miniApp.use('/movies', moviesRouter);
-miniApp.use('/movie-reviews', movie-reviewsRouter);
-miniApp.use('/add-movie', add-movieRouter);
+miniApp.use('/movie-reviews', movie_reviewsRouter);
+miniApp.use('/add-movie', add_movieRouter);
 miniApp.use('/movie', movieRouter);
-miniApp.use('/review' reviewRouter);
+miniApp.use('/review', reviewRouter);
 
 module.exports = miniApp;
